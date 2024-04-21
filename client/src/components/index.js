f0740bc92667c76fee988e918e246bea26f7c7b0
Prince <maihoonprinx@gmail.com>
2024-04-21 22:10:27 +0530

    just Storing import of every components at one place and exporting from here...

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,10 @@
 import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
+import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
+import EditProfile from "./EditProfile";
+import FriendsCard from "./FriendsCard";
+import PostCard from "./PostCard";
 
-export {CustomButton, TextInput, Loading };
+export {PostCard, FriendsCard, EditProfile, ProfileCard, TopBar, CustomButton, TextInput, Loading };
