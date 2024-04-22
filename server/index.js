f34841982cdd6df6a5aab3c98993f3b4a09a1d2b
Prince <maihoonprinx@gmail.com>
2024-04-22 19:11:25 +0530

    added PORT in index.js

--- server/index.js
+++ server/index.js
@@ -11,3 +11,16 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 8800;
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+app.listen(PORT, ()=>{
+    console.log(`Dev Server running on PORT ${PORT}`);
+});
